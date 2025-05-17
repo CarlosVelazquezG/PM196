f207eb0459bfbfaba9e773f67bb10a9ba9dffa30
CarlosVelazquezG <carlos.velazquez1235@outlook.com>
2025-05-16 21:56:58 -0600

    Ejercicio A
    Mejores prácticas en JS.

---
+++ Practica1/ejercicioLetConst.js
@@ -0,0 +1,15 @@
+//var nombre = "Armando";
+//var edad = 25;
+
+//nombre = "Ana María";
+
+//var saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+//console.log(saludo);
+
+let nombre = "Armando";
+const edad = 25;
+
+nombre = "Ana María";
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`;
+console.log(saludo);
