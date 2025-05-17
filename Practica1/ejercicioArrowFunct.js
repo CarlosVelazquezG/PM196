247efd80c834fbad86dd11b58b3326c998031073
CarlosVelazquezG <carlos.velazquez1235@outlook.com>
2025-05-16 22:04:42 -0600

    Ejercicio B
    Crear arrow function que haga lo mismo que la función previa.

---
+++ Practica1/ejercicioArrowFunct.js
@@ -0,0 +1,10 @@
+//function cuadrado(numero){
+//    return numero * numero;
+//}
+//console.log(cuadrado(5));
+
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(5));
+console.log(cuadrado(4));
+console.log(cuadrado(3));
