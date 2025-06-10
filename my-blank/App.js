8237a3fe45a5b8286c5f8a8006fdd83321f2cf4a
CarlosVelazquezG <carlos.velazquez1235@outlook.com>
2025-06-10 15:38:48 -0600

    Creación de Proyecto Blank y Texto casi Dinámico
    eso mismo del título

---
+++ my-blank/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto= ()=>{
+      return (
+        <Text> Hola mundo desde React Native </Text>
+      )
+}
+
+export default function App() {
+  return (
+
+    <View style={styles.container}>
+      <Texto></Texto>
+      <StatusBar style="auto" />
+      <Texto></Texto>
+      <Button title="Presionar"></Button>
+      <Texto></Texto>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
