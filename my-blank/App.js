9f8f79aee238e432c2794b2612251248bec3a9f0
CarlosVelazquezG <carlos.velazquez1235@outlook.com>
2025-06-13 16:06:14 -0600

    StateProps
    Uso de state con un botón y textos

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react'
 
 const Texto= ()=>{
+  const [contenido, setContenido]=useState('Hola Mundo')
+  const actualizarTexto=()=>{setContenido('Presionado')}
       return (
-        <Text> Hola mundo desde React Native </Text>
+        <Text onPress={actualizarTexto}>{contenido}</Text>
       )
 }
 
 export default function App() {
+  const [contBoton, setContBoton]=useState('Tócame')
+  const actualizarBoton=()=>{setContBoton('Tocado')}
   return (
 
     <View style={styles.container}>
       <Texto></Texto>
       <StatusBar style="auto" />
       <Texto></Texto>
-      <Button title="Presionar"></Button>
+      <Button title={contBoton} onPress={actualizarBoton}></Button>
       <Texto></Texto>
     </View>
   );
